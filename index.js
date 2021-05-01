f65e5a273795dd68861bd2eaf6b8d3d3522fade9
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-01 21:50:01 +1000

    Removed a duplicate function.

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you!"
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
     return "I can't hear you!";
   }
   if (string === string.toUpperCase()) {
